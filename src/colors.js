7df5251f5703b4e5a65f95a69d86180a933f67ab
Viktor Vsk <me@viktorvsk.com>
2023-02-04 19:41:15 +0100

    Colors

--- src/colors.js
+++ src/colors.js
@@ -5,14 +5,14 @@ export const secondaryColor = '#F7F7F7';
 export const textColor = '#121212';
 export const activeColor = '#0057B8';
 export const superActiveColor = '#FFDD00';
-export const priceColor = '#85bb65';
+export const disabledColor = '#d3d3d3';
 
 //
 
 export const spinnerColor = activeColor;
-export const disabledColor = '#d3d3d3';
 export const deletedColor = '#d43';
 export const errorColor = '#d43';
+export const priceColor = '#85bb65';
 
 //
 
