0e10c9ec083a064db8e96b55882f3e195c3eb44f
Viktor Vsk <me@viktorvsk.com>
2023-02-05 01:25:47 +0100

    Refactor AdScreen to functional component

--- src/components/Ad/AdScreen.js
+++ src/components/Ad/AdScreen.js
@@ -18,7 +18,7 @@ import {
   ActionSheet,
 } from 'native-base';
 
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import { loadAd } from 'actions/ads';
 
@@ -33,27 +33,32 @@ import { activeColor, textColor, spinnerColor, secondaryColor, primaryColor } fr
 
 import i18n from 'services/i18n';
 
-class AdScreen extends React.PureComponent {
-  shareAction = () => {
-    const { ad } = this.props;
+export default function AdScreen({
+  ad,
+  currentAdFriends,
+  askFriendsIsLoading,
+  isLoading,
+  actionsLoading,
+  onRefresh,
+  likeAd,
+  unlikeAd,
+  deleteAd,
+  archiveAd,
+  unarchiveAd,
+}) {
+  const { t } = useTranslation();
+  const refreshControl = <RefreshControl tintColor={spinnerColor} refreshing={isLoading} onRefresh={onRefresh} />;
+  const colorStyle = [styles.icon, ad.favorite ? styles.activeColor : styles.mainColor];
 
+  const shareAction = () =>
     Share.share({
       message: `${i18n.t('ad.shareText')} https://recar.io/ads/${ad.id}`,
       title: ad.title,
     });
-  };
-
-  t = this.props.t;
-
-  favAction = () => {
-    const { ad, likeAd, unlikeAd } = this.props;
-
-    ad.favorite ? unlikeAd(ad) : likeAd(ad);
-  };
-
-  deleteAd = () => {
-    const { t } = this.props;
-
+  const favAction = () => (ad.favorite ? unlikeAd(ad) : likeAd(ad));
+  const unarchiveAdAction = () => unarchiveAd(ad.id);
+  const editAd = () => Navigation.navigate('EditAdScreen', { ad: ad });
+  const deleteAdAction = () =>
     ActionSheet.show(
       {
         title: t('ad.alerts.confirm_delete'),
@@ -64,15 +69,11 @@ class AdScreen extends React.PureComponent {
       (buttonIndex) => {
         switch (buttonIndex) {
           case 0:
-            return this.props.deleteAd(this.props.ad.id);
+            return deleteAd(ad.id);
         }
       },
     );
-  };
-
-  archiveAd = () => {
-    const { t } = this.props;
-
+  const archiveAdAction = () =>
     ActionSheet.show(
       {
         title: t('ad.alerts.confirm_archive'),
@@ -83,137 +84,116 @@ class AdScreen extends React.PureComponent {
       (buttonIndex) => {
         switch (buttonIndex) {
           case 0:
-            return this.props.archiveAd(this.props.ad.id);
+            return archiveAd(ad.id);
         }
       },
     );
-  };
 
-  unarchiveAd = () => {
-    this.props.unarchiveAd(this.props.ad.id);
-  };
-
-  editAd = () => {
-    Navigation.navigate('EditAdScreen', { ad: this.props.ad });
-  };
-
-  render() {
-    const { t, ad, currentAdFriends, askFriendsIsLoading, isLoading, actionsLoading, onRefresh } = this.props;
-    const refreshControl = <RefreshControl tintColor={spinnerColor} refreshing={isLoading} onRefresh={onRefresh} />;
-    const colorStyle = [styles.icon, ad.favorite ? styles.activeColor : styles.mainColor];
-
-    if (isLoading && typeof ad.id === 'undefined') {
-      return (
-        <Container style={{ backgroundColor: primaryColor }}>
-          <Content>
-            <Spinner color={spinnerColor} />
-          </Content>
-        </Container>
-      );
-    }
-
+  if (isLoading && typeof ad.id === 'undefined') {
     return (
       <Container style={{ backgroundColor: primaryColor }}>
-        <View style={styles.headerBackground}>
-          <Header
-            noShadow={true}
-            iosBarStyle={Appearance.getColorScheme() === 'light' ? 'dark-content' : 'light-content'}
-            style={styles.header}
-          >
-            <Left>
-              <Icon
-                name={Platform.OS === 'android' ? 'arrow-back-circle-sharp' : 'chevron-back-circle-sharp'}
-                style={[styles.icon, { color: secondaryColor }]}
-                onPress={Navigation.popToTop}
-              />
-            </Left>
-            <Right style={styles.actionButtonsContainer}>
-              <Icon style={[styles.icon, { color: secondaryColor }]} onPress={this.shareAction} name="share-outline" />
-              <Icon
-                onPress={this.favAction}
-                style={colorStyle}
-                name={ad.favorite ? 'heart-circle-sharp' : 'heart-circle-outline'}
-              />
-            </Right>
-          </Header>
-        </View>
-        <ScrollView refreshControl={refreshControl} showsVerticalScrollIndicator={false}>
-          <ImageGallery
-            ad={ad}
-            imageStyle={{ height: 350, opacity: 0.75 }}
-            badgeStyle={{ right: 8, top: 312, position: 'absolute' }}
-            withModal={true}
-          />
-          {ad.images.length === 0 && <View style={styles.imagePlaceholder}></View>}
-          <View style={styles.contentContainer}>
-            {ad.my_ad && actionsLoading && <Spinner color={spinnerColor} />}
-
-            {ad.my_ad && !actionsLoading && (
-              <ScrollView
-                showsHorizontalScrollIndicator={false}
-                horizontal={true}
-                contentContainerStyle={styles.actions}
-              >
-                {!ad.deleted && (
-                  <Text style={styles.actionText} onPress={this.archiveAd}>
-                    <Icon style={styles.actionIcon} name="eye-off-outline" /> {t('actions.archive')}
-                  </Text>
-                )}
-                {ad.deleted && (
-                  <Text style={styles.actionText} onPress={this.unarchiveAd}>
-                    <Icon style={styles.actionIcon} name="eye-outline" /> {t('actions.unarchive')}
-                  </Text>
-                )}
-                <Text style={styles.actionText} onPress={this.deleteAd}>
-                  <Icon style={styles.actionIcon} name="trash-outline" /> {t('actions.delete')}
-                </Text>
-                <Text style={styles.actionText} onPress={this.editAd}>
-                  <Icon style={styles.actionIcon} name="create-outline" /> {t('actions.edit')}
-                </Text>
-              </ScrollView>
-            )}
-
-            <Text style={styles.title}>{ad.title}</Text>
-            <Text style={styles.price}>
-              {ad.price} {ad.category_currency}
-            </Text>
-
-            <View style={styles.oldPricesContainer}>
-              {ad.prices.map((v, index) => (
-                <Text key={v[1]} style={styles.priceVersion}>
-                  {v[1]} ${ad.prices.length === index + 1 ? null : ', '}
-                </Text>
-              ))}
-            </View>
-
-            {!ad.my_ad &&
-              (askFriendsIsLoading ? (
-                <Spinner color={spinnerColor} />
-              ) : (
-                <AskFriend ad={ad} currentAdFriends={currentAdFriends} />
-              ))}
-
-            {ad.deleted && (
-              <View style={styles.deletedContainer}>
-                <Text style={styles.deleted}>{t('ad.deleted')}</Text>
-              </View>
-            )}
-
-            {Object.keys(ad.options).length > 0 && <OptionsList ad={ad} />}
-
-            <View style={styles.descriptionContainer}>
-              <Text selectable style={styles.descriptionText}>
-                {ad.description}
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
+        <Content>
+          <Spinner color={spinnerColor} />
+        </Content>
       </Container>
     );
   }
-}
 
-export default withTranslation()(AdScreen);
+  return (
+    <Container style={{ backgroundColor: primaryColor }}>
+      <View style={styles.headerBackground}>
+        <Header
+          noShadow={true}
+          iosBarStyle={Appearance.getColorScheme() === 'light' ? 'dark-content' : 'light-content'}
+          style={styles.header}
+        >
+          <Left>
+            <Icon
+              name={Platform.OS === 'android' ? 'arrow-back-circle-sharp' : 'chevron-back-circle-sharp'}
+              style={[styles.icon, { color: secondaryColor }]}
+              onPress={Navigation.popToTop}
+            />
+          </Left>
+          <Right style={styles.actionButtonsContainer}>
+            <Icon style={[styles.icon, { color: secondaryColor }]} onPress={shareAction} name="share-outline" />
+            <Icon
+              onPress={favAction}
+              style={colorStyle}
+              name={ad.favorite ? 'heart-circle-sharp' : 'heart-circle-outline'}
+            />
+          </Right>
+        </Header>
+      </View>
+      <ScrollView refreshControl={refreshControl} showsVerticalScrollIndicator={false}>
+        <ImageGallery
+          ad={ad}
+          imageStyle={{ height: 350, opacity: 0.75 }}
+          badgeStyle={{ right: 8, top: 312, position: 'absolute' }}
+          withModal={true}
+        />
+        {ad.images.length === 0 && <View style={styles.imagePlaceholder}></View>}
+        <View style={styles.contentContainer}>
+          {ad.my_ad && actionsLoading && <Spinner color={spinnerColor} />}
+
+          {ad.my_ad && !actionsLoading && (
+            <ScrollView showsHorizontalScrollIndicator={false} horizontal={true} contentContainerStyle={styles.actions}>
+              {!ad.deleted && (
+                <Text style={styles.actionText} onPress={archiveAdAction}>
+                  <Icon style={styles.actionIcon} name="eye-off-outline" /> {t('actions.archive')}
+                </Text>
+              )}
+              {ad.deleted && (
+                <Text style={styles.actionText} onPress={unarchiveAdAction}>
+                  <Icon style={styles.actionIcon} name="eye-outline" /> {t('actions.unarchive')}
+                </Text>
+              )}
+              <Text style={styles.actionText} onPress={deleteAdAction}>
+                <Icon style={styles.actionIcon} name="trash-outline" /> {t('actions.delete')}
+              </Text>
+              <Text style={styles.actionText} onPress={editAdAction}>
+                <Icon style={styles.actionIcon} name="create-outline" /> {t('actions.edit')}
+              </Text>
+            </ScrollView>
+          )}
+
+          <Text style={styles.title}>{ad.title}</Text>
+          <Text style={styles.price}>
+            {ad.price} {ad.category_currency}
+          </Text>
+
+          <View style={styles.oldPricesContainer}>
+            {ad.prices.map((v, index) => (
+              <Text key={v[1]} style={styles.priceVersion}>
+                {v[1]} ${ad.prices.length === index + 1 ? null : ', '}
+              </Text>
+            ))}
+          </View>
+
+          {!ad.my_ad &&
+            (askFriendsIsLoading ? (
+              <Spinner color={spinnerColor} />
+            ) : (
+              <AskFriend ad={ad} currentAdFriends={currentAdFriends} />
+            ))}
+
+          {ad.deleted && (
+            <View style={styles.deletedContainer}>
+              <Text style={styles.deleted}>{t('ad.deleted')}</Text>
+            </View>
+          )}
+
+          {Object.keys(ad.options).length > 0 && <OptionsList ad={ad} />}
+
+          <View style={styles.descriptionContainer}>
+            <Text selectable style={styles.descriptionText}>
+              {ad.description}
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </Container>
+  );
+}
 
 AdScreen.propTypes = {
   ad: PropTypes.object.isRequired,
