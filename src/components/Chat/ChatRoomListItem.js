6433f7daa5158c76e4c3640508c9922133d467a2
Viktor Vsk <me@viktorvsk.com>
2023-02-26 23:07:30 +0100

    Swipeable ChatRoomListItem

--- src/components/Chat/ChatRoomListItem.js
+++ src/components/Chat/ChatRoomListItem.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, Animated } from 'react-native';
 
 import Navigation from 'services/Navigation';
 
@@ -12,13 +12,26 @@ import dayjs from 'dayjs';
 
 import { useTranslation } from 'react-i18next';
 
-import { activeColor, disabledColor, secondaryColor, superActiveColor, textColor, activeTextColor } from 'colors';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { RectButton } from 'react-native-gesture-handler';
+
+import {
+  activeColor,
+  disabledColor,
+  secondaryColor,
+  superActiveColor,
+  textColor,
+  activeTextColor,
+  deletedColor,
+  primaryColor,
+} from 'colors';
 
 import { leaveChat } from 'actions/chat';
 
 import RECARIO_LOGO from 'assets/logo.png';
 
 export default function ChatRoomListItem({ chat, currentUser }) {
+  const swipeableRef = useRef({});
   const { t, i18n } = useTranslation();
   const dispatch = useDispatch();
   const lastMessage = chat.messages[0];
@@ -34,7 +47,7 @@ export default function ChatRoomListItem({ chat, currentUser }) {
   let messagePreview = lastMessage ? lastMessage.text.replace(/\n/g, ' ') : '';
   messagePreview = messagePreview.length > 20 ? `${messagePreview.substring(0, 20)}...` : messagePreview;
 
-  const onLongPress = () => {
+  const onLeave = useCallback(() => {
     const msg = lastMessage.user?._id ? `${lastMessage.user.name}: ${messagePreview}` : messagePreview;
     const title = chat.system ? t('chat.actions.systemChatHeader') : `${chat.title}\n${lastMessageDateString} – ${msg}`;
 
@@ -46,46 +59,79 @@ export default function ChatRoomListItem({ chat, currentUser }) {
         destructiveButtonIndex: 0,
       },
       (buttonIndex) => {
-        buttonIndex === 0 && dispatch(leaveChat(chat.id));
+        buttonIndex === 0 && dispatch(leaveChat(chat.id)) && swipeableRef.current?.close();
       },
     );
-  };
+  }, [chat.id]);
+
+  const renderLeftActions = useCallback(
+    (progress, dragX) => {
+      const trans = dragX.interpolate({
+        inputRange: [0, 50, 100, 101],
+        outputRange: [-20, 0, 0, 1],
+      });
+      return (
+        <RectButton style={styles.leftAction} onPress={onLeave}>
+          <Animated.Text
+            style={[
+              styles.actionText,
+              {
+                transform: [{ translateX: trans }],
+              },
+            ]}
+          >
+            {t('chat.actions.leave')}
+          </Animated.Text>
+        </RectButton>
+      );
+    },
+    [chat.id],
+  );
 
   return (
-    <ListItem
-      noBorder
-      noIndent
-      thumbnail
-      activeOpacity={1}
-      underlayColor="transparent"
-      onPress={onPress}
-      onLongPress={onLongPress}
-      style={styles.chatRow}
+    <Swipeable
+      friction={2}
+      enableTrackpadTwoFingerGesture
+      leftThreshold={30}
+      rightThreshold={40}
+      ref={(ref) => (swipeableRef.current = ref)}
+      renderLeftActions={renderLeftActions}
     >
-      <Left>
-        {chat.system ? (
-          <Image source={RECARIO_LOGO} style={styles.systemLogoImage} />
-        ) : (
-          <UserAvatar size={48} name={chat.title || ''} src={chat.photo} bgColor={activeColor} />
-        )}
-      </Left>
-      <Body style={styles.previewBody}>
-        <Text style={styles.title}>{chat.system ? t('chat.systemChatTitle') : chat.title}</Text>
-        <Text style={styles.textColor}>
-          {lastMessage?.user?._id && (lastMessage.user._id === currentUser._id ? t('chat.you') : lastMessage.user.name)}
-        </Text>
+      <ListItem
+        noBorder
+        noIndent
+        thumbnail
+        activeOpacity={1}
+        underlayColor="transparent"
+        onPress={onPress}
+        style={styles.chatRow}
+      >
+        <Left>
+          {chat.system ? (
+            <Image source={RECARIO_LOGO} style={styles.systemLogoImage} />
+          ) : (
+            <UserAvatar size={48} name={chat.title || ''} src={chat.photo} bgColor={activeColor} />
+          )}
+        </Left>
+        <Body style={styles.previewBody}>
+          <Text style={styles.title}>{chat.system ? t('chat.systemChatTitle') : chat.title}</Text>
+          <Text style={styles.textColor}>
+            {lastMessage?.user?._id &&
+              (lastMessage.user._id === currentUser._id ? t('chat.you') : lastMessage.user.name)}
+          </Text>
 
-        <Text style={styles.smallFont}>{messagePreview}</Text>
-      </Body>
-      <Right style={styles.notes}>
-        <Text style={[styles.smallFont, styles.centered]}>{lastMessageDateString}</Text>
-        {chat.new_messages_count > 0 && (
-          <Badge style={styles.unreadBadge}>
-            <Text style={styles.activeTextColor}>{chat.new_messages_count}</Text>
-          </Badge>
-        )}
-      </Right>
-    </ListItem>
+          <Text style={styles.smallFont}>{messagePreview}</Text>
+        </Body>
+        <Right style={styles.notes}>
+          <Text style={[styles.smallFont, styles.centered]}>{lastMessageDateString}</Text>
+          {chat.new_messages_count > 0 && (
+            <Badge style={styles.unreadBadge}>
+              <Text style={styles.activeTextColor}>{chat.new_messages_count}</Text>
+            </Badge>
+          )}
+        </Right>
+      </ListItem>
+    </Swipeable>
   );
 }
 
@@ -109,9 +155,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   chatRow: {
-    marginBottom: 8,
+    paddingBottom: 8,
     borderBottomWidth: 1,
     borderBottomColor: secondaryColor,
+    flex: 1,
+    backgroundColor: primaryColor,
   },
   centered: {
     alignSelf: 'center',
@@ -130,5 +178,17 @@ const styles = StyleSheet.create({
     paddingRight: 6,
     paddingBottom: 2,
     justifyContent: 'space-between',
+  },
+
+  leftAction: {
+    backgroundColor: deletedColor,
+    justifyContent: 'center',
+    flex: 1,
+  },
+  actionText: {
+    color: 'white',
+    fontSize: 16,
+    backgroundColor: 'transparent',
+    padding: 10,
   },
 });
